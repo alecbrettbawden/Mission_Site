e8e6ec3330eb6b6a63731917e81da5bde6230650
spovar <41087939+spovar@users.noreply.github.com>
2018-11-10 09:30:01 -0700

    Added bootstrap, entity framework and edited mission controller

---
+++ Mission_Site/Controllers/MissionController.cs
@@ -0,0 +1,33 @@
+﻿using Mission_Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mission_Site.Controllers
+{
+    public class MissionController : Controller
+    {
+
+        public static List<Mission> missionList = new List<Mission>();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*[HttpPost]
+        public ActionResult NewMission(Mission newMission)
+        {
+            missionList.Add(newMission);
+            return View();
+        }*/
+
+        public ActionResult MissionFAQ(int missionID)
+        {
+            return View(missionList)
+        }
+    }
+}
