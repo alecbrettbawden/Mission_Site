bb7b4e6c508adf7d1a3600c9c1446e7392f69ddf
spovar <41087939+spovar@users.noreply.github.com>
2018-11-10 08:55:21 -0700

    Added a mission model

---
+++ Mission_Site/Models/Mission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mission_Site.Models
+{
+    public class Mission
+    {
+        public int missionID { get; set; }
+        public string missionName { get; set; }
+        public string missionPresidentName { get; set; }
+        public string missionStreetAddress { get; set; }
+        public string missionCity { get; set; }
+        public int missionZip { get; set; }
+        public string? missionState { get; set; }
+        public string missionCountry { get; set; }
+    }
+}
